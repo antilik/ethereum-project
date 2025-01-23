e35e1abebbe1648355b41a8dd1b19fad57c4c89b
Anton Degnera <anton.degnera@gmail.com>
2025-01-22 22:30:58 -0400

    feat: added tests "manager" and "contributor".

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -9,20 +9,22 @@ const compileFactory = require('../ethereum/build/CampaignFactory.json');
 const compileCampaign = require('../ethereum/build/Campaign.json');
 
 let accounts;
+let managerAccount;
 let factory;
 let campaignAddress;
 let campaign;
 
 beforeEach(async () => {
   accounts = await web3.eth.getAccounts();
+  managerAccount = accounts[0];
 
   factory = await new web3.eth.Contract(JSON.parse(compileFactory.interface))
     .deploy({ data: compileFactory.bytecode })
-    .send({ from: accounts[0], gas: '1000000'});
+    .send({ from: managerAccount, gas: '1000000'});
 
   await factory.methods.createCampaign('100')
     .send({
-      from: accounts[0],
+      from: managerAccount,
       gas: '1000000',
     });
 
@@ -39,5 +41,21 @@ describe('Campaign', () => {
   it('Should deploy a factory and a campaign', async () => {
     assert.ok(factory.options.address);
     assert.ok(campaign.options.address);
+  });
+
+  it('Should mark caller as campaign manager', async () => {
+    const manager = await campaign.methods.manager().call();
+    assert.equal(managerAccount, manager);
+  });
+
+  it('Should allow people to contribute $ and marks them as approvers', async () => {
+    const contributor = accounts[1];
+    await campaign.methods.contribute().send({
+      value: '200',
+      from: contributor,
+    });
+
+    const isContributor = await campaign.methods.approvers(contributor).call();
+    assert(isContributor);
   });
 });
