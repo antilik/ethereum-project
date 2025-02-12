764cc5f9f821a1fbfea93f68b7b44303df8897db
Anton Degnera <anton.degnera@gmail.com>
2025-02-11 23:17:32 -0400

    feat: added handling form, error message and spinner.

--- pages/campaigns/new.jsx
+++ pages/campaigns/new.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Form, Button, Input } from 'semantic-ui-react';
+import React, {useState} from 'react';
+import { Form, Button, Input, Message } from 'semantic-ui-react';
 
 import Layout from '../../components/Layout';
 // web3 components
@@ -8,20 +8,31 @@ import web3 from '../../ethereum/web3';
 
 
 const CampaignNew = () => {
-  const [minimumContribution, setMinimumContribution] = React.useState(0);
+  const [minimumContribution, setMinimumContribution] = useState(0);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const accounts = await web3.eth.getAccounts();
-    await factory.methods.createCampaign(minimumContribution)
-      .send({
-        from:accounts[0],
-      })
+    setIsLoading(true);
+    setErrorMessage(''); // cleanup
+
+    try {
+      const accounts = await web3.eth.getAccounts();
+      await factory.methods.createCampaign(minimumContribution)
+        .send({
+          from:accounts[0],
+        });
+    } catch (err) {
+      setErrorMessage(err.message);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
     <Layout>
       <h3>Create Campaign</h3>
-      <Form onSubmit={handleSubmit}>
+      <Form onSubmit={handleSubmit} error={!!errorMessage}>
         <Form.Field>
           <label htmlFor="">Minimum Contribution</label>
           <Input
@@ -31,11 +42,12 @@ const CampaignNew = () => {
             value={minimumContribution}
             onChange={(e) => {
               setMinimumContribution(e.target.value);
+              errorMessage && setErrorMessage(''); // cleanup
             }}
           />
-
         </Form.Field>
-        <Button primary>Create</Button>
+        <Message error header="Error" content={errorMessage}/>
+        <Button primary loading={isLoading}>Create</Button>
       </Form>
     </Layout>
   )
